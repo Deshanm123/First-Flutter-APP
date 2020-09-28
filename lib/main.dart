9fa0810dfd57f6057013aae9a3c47fff0c428a92
Deshan <deshanm123@gmail.com>
2020-09-28 20:59:57 +0530

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+  runApp(
+    Center(
+      child:Text("Hello World! This is my first Flutter Application", textDirection :TextDirection.ltr,),
+    )
+  );
+
+
+}
